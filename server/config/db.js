fe93ebce97a1c27c86795fa9ab8e87f62c6bc50f
James Kuzhilaparambil <rhinosaur2000@gmail.com>
2021-10-18 18:18:03 -0400

    updated db to mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,6 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://james:9JfkTgdZ4ns5c3X5@assignment2db.pswej.mongodb.net/Business_Contacts?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
